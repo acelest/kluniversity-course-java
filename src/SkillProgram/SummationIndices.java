720dd0e40048717701c6c91d25db70cdcfe1967c
acelest <eyengadjou@gmail.com>
2025-01-06 17:05:50 +0530

    week 4 skill program complete

--- src/SkillProgram/SummationIndices.java
+++ src/SkillProgram/SummationIndices.java
@@ -2,21 +2,24 @@ package SkillProgram;
 
 public class SummationIndices {
 
-    // Méthode pour trouver les indices
     public static int[] findIndices(int[] numbers, int target) {
-        for (int i = 0; i < numbers.length; i++) {
-            for (int j = i + 1; j < numbers.length; j++) {
-                if (numbers[i] + numbers[j] == target) {
+
+        for (int i=0; i<numbers.length; i++) {
+            for (int j=1; j<numbers.length;j++) {
+                if (numbers[i]+numbers[j] == target){
                     return new int[]{i, j};
                 }
             }
         }
-        return new int[]{-1, -1}; // Si aucune combinaison trouvée
+
+
+        return new int[] {-1,1};
     }
 
     public static void main(String[] args) {
-        int[] numbers = {1, 2, 3, 4};
-        int target = 9;
+        int[]numbers={1,3,4,5,6};
+
+        int target = 5;
 
         int[] result = findIndices(numbers, target);
 
