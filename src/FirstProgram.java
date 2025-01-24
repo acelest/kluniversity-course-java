8d49083d80d215ba1c853cf2b0da0f380cb5ecab
acelestdev <eyengadjou@gmail.com>
2025-01-24 12:48:03 +0530

    Implement findMinMax method and enhance user input handling in FirstProgram.java

--- src/FirstProgram.java
+++ src/FirstProgram.java
@@ -1,9 +1,42 @@
-public class FirstProgram {
-    public static void main(String[] args){
-        System.out.print("enter your age");
-//        System.out.println("i'm aubin djou, i already learning java ");
+import java.util.*
+
+class Find {
+    public static void findMinMax (int [] number, int [] result) {
+        int min=Integer.MIN_VALUE
+        int max=Integer.MAX_VALUE
+    }
+    for (int num: numbers) {
+        if (num < min ) {
+            min=num
+        }
+        if (num > min )
+            max=num
     }
 }
-//public class aubinDjou {
-//    System.out.print("enter your age");
-//}
+
+result [0]=min
+result[1]=max
+
+public static void mai (String[]args) {
+    Scaner sc= new Scanner (system.in)
+    s.o.p ( "enter a number of elements")
+    int n = sc.nextInt ()
+    int [] number = new int [n]
+    s.op ("enter a element")
+
+    for (i=0, i<n, i++) {
+        number[i]=sc.nextInt()
+    }
+
+    int []result = new int [2];
+    findMinMax(numbers,result)
+
+    s.o.p ("a smalleest number is"+ result[0])
+    s.o.p ("a biggest number is " + result[1])
+}
+
+Scaner sc = new Scanner (system.in)
+sop
+int n = sc.nextInt()
+int [] number =new int [2]
+fit
